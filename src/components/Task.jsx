cdbcdf8e71ba33a59a1063434efaf7cf16a4299a
Jose A. Pinell <japinell@yahoo.com>
2025-05-25 12:22:36 -0700

    Added properties to Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,10 +2,40 @@ import React from "react";
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
   return (
-    <div classname="list-item">
-      <label htmlFor="title" aria-label={title}>
-        <input type="text" value={title} readOnly={true} name="title" />
+    <div className={`list-item ${state}`}>
+      <label
+        htmlFor="checked"
+        aria-label={`archiveTask-${id}`}
+        className="checkbox"
+      >
+        <input
+          type="checkbox"
+          disabled={true}
+          name="checked"
+          id={`archiveTask-${id}`}
+          checked={state === "TASK_ARCHIVED"}
+        />
       </label>
+      <label htmlFor="title" aria-label={title} className={title}>
+        <input
+          type="text"
+          value={title}
+          readOnly={true}
+          name="title"
+          placeholder="Enter task title..."
+        />
+      </label>
+      {state !== "TASK_ARCHIVED" && (
+        <button
+          className="pin-button"
+          onClick={() => onPinTask(id)}
+          id={`pinTask-${id}`}
+          aria-label={`pinTask-${id}`}
+          key={`pinTask-${id}`}
+        >
+          <span className={`icon-star`}></span>
+        </button>
+      )}
     </div>
   );
 };
